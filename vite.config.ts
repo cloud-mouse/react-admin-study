30f2051301007dec53bdd1d672bcc5fdf8ad3b2d
昊 张 <zhanghao316@outlook.com>
2025-02-10 17:49:59 +0800

    Enhance Vite config with build optimizations and asset handling

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,32 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  }
+  },
+  build: {
+    outDir: 'dist', // 相对于根目录的构建输出位置。如果目录存在，则将在构建之前删除该目录。
+    minify: 'esbuild', // 混淆器，terser 构建后文件体积更小，'boolean' | 'terser' | 'esbuild'
+    reportCompressedSize: true,
+    // 规定触发警告的 chunk 大小.默认500
+    // 消除打包大小超过500kb警告
+    chunkSizeWarningLimit: 4000,
+    rollupOptions: {
+      // 静态资源分类打包
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().match(/\/node_modules\/(?!.pnpm)(?<moduleName>[^/]*)\//)?.groups!.moduleName ?? 'vender'
+          }
+        },
+      },
+    },
+  },
 })
