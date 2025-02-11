a94f85eaf3b44109001234877a78a50e2d9c18be
昊 张 <zhanghao316@outlook.com>
2025-02-11 11:43:09 +0800

    Add route protection and Navigate for unauthorized access

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // App.tsx
 import React from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import Layout from './layouts/Layout'
 import routes from './routes/index'
+import { useAppSelector } from './store/hooks'
 
 const App: React.FC = () => {
+  const { isLoggedIn } = useAppSelector((state) => state.user)
   return (
     <Provider store={store}>
       <Router>
@@ -14,12 +16,16 @@ const App: React.FC = () => {
           {/* 登录路由单独配置，不使用Layout */}
           <Route path="/login" element={routes.find((route) => route.path === '/login')?.element} />
 
-          {/* 其他路由使用Layout布局 */}
-          {routes
-            .filter((route) => route.path !== '/login')
-            .map((route, index) => (
-              <Route key={index} path={route.path} element={<Layout>{route.element}</Layout>} />
-            ))}
+          {/* 路由拦截 */}
+          {isLoggedIn ? (
+            // 如果已登录，允许访问其他路由
+            routes
+              .filter((route) => route.path !== '/login')
+              .map((route, index) => <Route key={index} path={route.path} element={<Layout>{route.element}</Layout>} />)
+          ) : (
+            // 如果未登录，重定向到登录页
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          )}
         </Routes>
       </Router>
     </Provider>
